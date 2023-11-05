d40e5b0fd045b8d40586d845f4ab383514e46235
pedroelir <pedro_r_b777@hotmail.com>
2023-11-05 14:28:01 +0100

    upload black and pylint basic examples

---
+++ black_example.py
@@ -0,0 +1,17 @@
+
+def foo():
+
+    print("All the newlines above me should be deleted!")
+
+
+if condition:
+
+    print("No newline above me!")
+
+    print("There is a newline above me, and that's OK!")
+
+
+class Point:
+
+    x: int
+    y: int
